6956ad65e7171dcab2b6e5c4e6c9507514528408
GonzaloE <errandoneagonzalo18@gmail.com>
2023-06-19 22:37:53 -0300

    Arreglado, ahora elimina correctamente

--- src/Routes/Service/delLocationService.js
+++ src/Routes/Service/delLocationService.js
@@ -5,12 +5,12 @@ const delLocationService = async (req, res) =>
 
     try {
 
-        const id = req.body.id
+        const id = req.params.id
 
         const response = await delLocation(id)
 
         if(response)
-            res.status(response.status).send(response)
+            res.status(200).send(response)
         else
             throw `error, la ubicacion con id "${id}" es inexistente`
     } catch (err) 
